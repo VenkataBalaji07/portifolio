6fd0e401585e0cb3680d58d8cf43907f3df52591
Mohan Sai <mohansaichinthala9121@gmail.com>
2025-03-02 23:09:47 +0530

    basic contact layout added

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,15 +1,30 @@
-import React from 'react'
+import contact3D from "../../assets/3d-contact.webp";
+import React from "react";
+import "./Contact.css";
 
 const Contact = () => {
   return (
     <div>
-      <center>
-        <h1>
-            Contact Me
-        </h1>
-      </center>
+        <center>
+            <h1>Contact Me</h1>
+        </center>
+        <div className="contactLayout">
+        <div>
+        <img src={contact3D} alt="contact image" className="contactImage" />
+      </div>
+      <div className="contactForm">
+        <form>
+            <label>Name:</label>
+            <input type="text" /><br/>
+            <label>Email:</label>
+            <input type="text" /><br/>
+            <label>Message:</label>
+            <textarea rows={4}/>
+        </form>
+      </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
