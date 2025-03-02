acc6c7dc0b30b98adcaec5fee73e8875cfd5f5d1
Mohan Sai <mohansaichinthala9121@gmail.com>
2025-03-02 16:24:14 +0530

    added education details

--- src/Pages/Education/Education.jsx
+++ src/Pages/Education/Education.jsx
@@ -1,11 +1,56 @@
+import khit from '../../assets/khit-logo.png';
+import nagarjunaSchool from '../../assets/nagarjuna-school-logo.jpeg';
+import sims from '../../assets/sims-logo.jpg';
 import React from 'react'
+import './Education.css'
 
 const Education = () => {
   return (
     <div>
       <center>
-        <h1>Education</h1>
+        <h1>Education Details</h1>
       </center>
+      <div className='collegesDisplay'>
+        <div className='collegeDetails'>
+            <div>
+                <h3>Kallam Haranadhareddy Institute of Technology</h3>
+                <div className='studyDetails'>
+                <p>Btech in AIML</p>
+                <p>2020 - 2024</p>
+                <p>CGPA: 7.16 / 10</p>
+                </div>
+            </div>
+            <div>
+                <img src={khit} className='collegeImages' />
+            </div>
+        </div>
+        <div className='collegeDetails'>
+            <div>
+                <h3>SIMS Junior College</h3>
+                <div className='studyDetails'>
+                <p>Intermediate in MPC</p>
+                <p>2018 - 2020</p>
+                <p>CGPA: 7.45 / 10</p>
+                </div>
+            </div>
+            <div>
+                <img src={sims} className='collegeImages' />
+            </div>
+        </div>
+        <div className='collegeDetails'>
+            <div>
+                <h3>Nagarjuna High School</h3>
+                <div className='studyDetails'>
+                <p>10th Class</p>
+                <p>2017 - 2018</p>
+                <p>CGPA: 8.8 / 10</p>
+                </div>
+            </div>
+            <div>
+                <img src={nagarjunaSchool} className='collegeImages' />
+            </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -5,6 +5,7 @@ import Contact from "../Contact/Contact";
 import baluPic from "../../assets/balu-photo.jpg";
 import { IoBookSharp } from "react-icons/io5";
 import './Home.css';
+import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -18,13 +19,19 @@ const Home = () => {
       </div>
       <div className="titleSection" >
         <div className="titleSectionLeft">
-        <h1>Balu</h1>
+        <h1>Venkata Balaji Boppudi</h1>
         <h3>Software Developer | Problem Solver | Tech Enthusiast</h3>
         <p>Passionate about building scalable and efficient software solutions that enhance user experiences. I thrive on solving complex problems through clean, maintainable code and innovative thinking. Constantly exploring new technologies to stay ahead in the ever-evolving world of software development.</p>
         </div>
         <div>
             <img src={baluPic} alt="Balu" className="profilePic" />
         </div>
+      </div>
+      <div className="headSectionButtons">
+        <button><a href="https://github.com/VenkataBalaji07" target="_blank" style={{color:"white"}}><FaGithub/></a></button>
+        <button><a href="https://www.linkedin.com/in/venkata-balaji-boppudi-632b5b248" target="_blank" style={{color:"white"}}><FaLinkedin/></a></button>
+        <button><a href="https://x.com/Balaji4207?t=NGov0PEfCcGWFL_1G1WlEg&s=09" target="_blank" style={{color:"white"}}><FaTwitter/></a></button>
+        <button>Resume</button>
       </div>
       <Education />
       <Projects />
